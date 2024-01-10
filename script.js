8e9a67598cf053629a15abcd9f164aca425907e9
Franco Fortunatto <50719030+TheFortunattor@users.noreply.github.com>
2024-01-10 14:13:14 -0300

    Added func to get the computer choice of rps

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Test");
+function getComputerChoice() {
+    //Let the computer choose a number between 1 and 3
+    let computerChoice = Math.floor(Math.random() * 3) + 1;
+    //Convert the result from computerChoice into either rock, paper or scissors
+    switch(computerChoice) {
+        case 1:
+            computerChoice = "Rock";
+            break;
+        case 2:
+            computerChoice = "Paper";
+            break;
+        case 3:
+            computerChoice = "Scissors";
+            break;
+    }
+    //Return the resulting value!
+    return computerChoice;
+}
+
