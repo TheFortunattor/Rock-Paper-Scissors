75f8d2b275e5cd5546e5470b634920781c161038
Franco Fortunatto <50719030+TheFortunattor@users.noreply.github.com>
2024-01-11 12:30:42 -0300

    Implemented a funcion to make sure the user input is correct

--- script.js
+++ script.js
@@ -19,17 +19,51 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        return "Empate!";
+        return "It's a tie!";
     }
     else if (playerSelection === "Rock" && computerSelection === "Scissors" || playerSelection === "Paper" && computerSelection === "Rock" || playerSelection === "Scissors" && computerSelection === "Paper") {
-        return "You win! "+ playerSelection + " beats " + computerSelection + "!"
+        return "You win! " + playerSelection + " beats " + computerSelection + "!"
     }
     else {
         return "You lose! " + computerSelection + " beats " + playerSelection + "!"
     }
 }
+function capitalizeInput(playerInput) {
+    //First, we make the whole thing lowercase
+    playerInput = playerInput.toLowerCase();
+    //Then I use charAt(0) to grab the first letter of the word
+    const firstLetter = playerInput.charAt(0);
+    //Capitalize the first letter we extracted
+    const firstLetterCap = firstLetter.toUpperCase();
+    //Grab the rest of the word past the first letter
+    const remainingLetters = playerInput.slice(1);
+    //Put it all together
+    const capitalizedWord = firstLetterCap + remainingLetters;
+    return capitalizedWord;
+}
 
-const playerSelection = "Rock";
+function checkPlayerInput(playerInput) {
+    let keepGoing = true;
+    while(keepGoing) {
+        playerInput = capitalizeInput(playerInput);
+        console.log("value before IF statement:")
+        console.log(playerInput);
+        if (playerInput != "Rock" && playerInput != "Paper" && playerInput != "Scissors") {
+            playerInput = prompt("Wrong data! Please write either rock, paper or scissors!");
+            console.log("Value AFTER if:")
+            console.log(playerInput);
+        }
+        else {
+            keepGoing = false;
+        }
+    }
+    return playerInput;
+}
+
+let playerSelection = prompt("Type rock paper or Scissors: ");
+// playerSelection = capitalizeInput(playerSelection);
+playerSelection = checkPlayerInput(playerSelection);
+
 const computerSelection = getComputerChoice();
 
 console.log("Player choice:");
@@ -38,5 +72,5 @@ console.log(playerSelection);
 console.log("Computer choice:")
 console.log(computerSelection);
 
-console.log("Juego:")
-console.log(playRound(playerSelection, computerSelection));
+// console.log("Juego:")
+// console.log(playRound(playerSelection, computerSelection));
